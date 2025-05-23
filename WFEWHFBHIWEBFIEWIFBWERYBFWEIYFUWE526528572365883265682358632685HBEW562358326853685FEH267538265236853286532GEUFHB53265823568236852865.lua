9e1c512b87bf71f03224d5c2fc149161502090c0
yougotonesickfishlol53454354343 <givemetrialcreditslol@gmail.com>
2025-05-23 10:47:58 -0600

    Update WFEWHFBHIWEBFIEWIFBWERYBFWEIYFUWE526528572365883265682358632685HBEW562358326853685FEH267538265236853286532GEUFHB53265823568236852865.lua

--- WFEWHFBHIWEBFIEWIFBWERYBFWEIYFUWE526528572365883265682358632685HBEW562358326853685FEH267538265236853286532GEUFHB53265823568236852865.lua
+++ WFEWHFBHIWEBFIEWIFBWERYBFWEIYFUWE526528572365883265682358632685HBEW562358326853685FEH267538265236853286532GEUFHB53265823568236852865.lua
@@ -2449,11 +2449,12 @@ local function CXQY_fake_script() -- AutofarmButton.ButtonManager
 			print("AUTOFARM ON")
 			ESB.Image = "http://www.roblox.com/asset/?id=6670322349"
 			local player = game.Players.LocalPlayer
-	
+	wait(1)
+				player.Character.HumanoidRootPart.CFrame = Vector3.new(-285, 511, -1486)
 			while on == true do
 	
 	
-				local spawner = game.Map["Farming Zone"]:FindFirstChild("EquipmentSpawner")
+				local spawner = game.Workspace.Map["Farming Zone"]:FindFirstChild("EquipmentSpawner")
 if not spawner then
 	warn("Spawner not found in Farming Zone")
 	return
